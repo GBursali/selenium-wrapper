1472523468c6c2c4001b52a9921485510deaf93c
gurkanbursali <gurkan.bursali@testinium.com>
2024-03-07 12:31:58 +0300

    maven distro added

--- src/main/java/space/gbsdev/core/Utilities.java
+++ src/main/java/space/gbsdev/core/Utilities.java
@@ -2,6 +2,7 @@ package space.gbsdev.core;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 import org.junit.Assume;
 
 import java.io.IOException;
@@ -11,16 +12,39 @@ import java.nio.file.Path;
 public class Utilities {
 
     private Utilities(){}
-    public static String getFileContents(Path file){
-        Assume.assumeTrue("Requested file is not found: " + file,file.toFile().exists());
+
+    /**
+     * Reads the contents of a file and returns them as a string.
+     *
+     * @param file The path to the file to read.
+     * @return The contents of the file as a string.
+     * @throws IllegalArgumentException If the file does not exist or if there is a problem reading the file.
+     */
+    public static String getFileContents(Path file) {
+        if (!Files.exists(file))
+            throw new IllegalArgumentException("Requested file is not found: " + file);
+
         try {
             return Files.readString(file);
         } catch (IOException e) {
-            throw new IllegalArgumentException("There is a problem in reading the file: " + file);
+            throw new IllegalArgumentException("There is a problem in reading the file: " + file, e);
         }
     }
 
-    public static JsonObject getJsonFile(Path file){
-        return JsonParser.parseString(getFileContents(file)).getAsJsonObject();
+    /**
+     * Reads the contents of a JSON file and parses it into a JsonObject.
+     *
+     * @param file The path to the JSON file to read.
+     * @return The contents of the JSON file as a JsonObject.
+     * @throws IllegalArgumentException If the file does not exist, if there is a problem reading the file, or if there is a syntax error in the JSON content.
+     */
+    public static JsonObject getJsonFile(Path file) {
+        try {
+            String fileContents = getFileContents(file);
+            return JsonParser.parseString(fileContents).getAsJsonObject();
+        } catch (JsonSyntaxException e) {
+            throw new IllegalArgumentException("There is a syntax error in the JSON file: " + file, e);
+        }
     }
+
 }
